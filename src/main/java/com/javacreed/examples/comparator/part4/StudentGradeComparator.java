bf73c705e7cdc13527a4ef5dafeefc96df81e844
javacreed <albert@javacreed.com>
2015-03-14 15:02:03 +0100

    Initial Import

---
+++ src/main/java/com/javacreed/examples/comparator/part4/StudentGradeComparator.java
@@ -0,0 +1,41 @@
+/*
+ * #%L
+ * Comparing the Performance of some List Implementations
+ * $Id:$
+ * $HeadURL:$
+ * %%
+ * Copyright (C) 2012 - 2015 Java Creed
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package com.javacreed.examples.comparator.part4;
+
+import java.util.Comparator;
+
+public class StudentGradeComparator implements Comparator<Student> {
+
+  public static StudentGradeComparator getInstance() {
+    return StudentGradeComparator.instance;
+  }
+
+  private static final StudentGradeComparator instance = new StudentGradeComparator();
+
+  private StudentGradeComparator() {}
+
+  @Override
+  public int compare(final Student a, final Student b) {
+    return a.getGrade() - b.getGrade();
+  }
+
+}
